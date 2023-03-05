8db0540f18af2debd6f18a1cd7c0d7164afc5f00
Javier <javier96rn@gmail.com>
2023-03-05 17:06:07 +0100

    Add route for the campgrounds index

--- index.js
+++ index.js
@@ -22,16 +22,10 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-// app.get('/makecampground', async (req, res) => {
-//   const camp = new Campground({
-//     title: 'My backyard',
-//     price: '25.99',
-//     description: 'fajsdfljasdf ñlasjdfñlasdj flñasdjfasd',
-//     location: 'Palm Springs',
-//   });
-//   await camp.save();
-//   res.send(camp);
-// });
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
