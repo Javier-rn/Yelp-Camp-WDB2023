1db88b92ee9b8f420456a703c5842d042d14e16e
Javier-rn <javier96rn@gmail.com>
2023-04-01 15:44:18 +0200

    Add connect-flash

--- routes/campground.js
+++ routes/campground.js
@@ -61,6 +61,7 @@ router.post(
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Succesfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
