2bbcaf5ca9e556000486ffed1f940a327f139b9e
Javier-rn <javier96rn@gmail.com>
2023-04-07 16:05:52 +0200

    Add config for passport-local-mongoose

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campground');
 const reviews = require('./routes/reviews');
@@ -44,6 +47,13 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
