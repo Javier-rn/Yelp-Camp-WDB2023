cefd0f36eef2e598262c6429c7a378885b097711
Javier-rn <javier96rn@gmail.com>
2023-04-10 12:38:05 +0200

    Add backend authorization logic in edit routes

--- routes/campground.js
+++ routes/campground.js
@@ -52,6 +52,10 @@ router.get(
       req.flash('error', 'Cannot find that campground');
       return res.redirect('/campgrounds');
     }
+    if (!campground.author.equals(req.user._id)) {
+      req.flash('error', 'You do not have permission to do that!');
+      return res.redirect(`/campgrounds/${id}`);
+    }
     res.render('campgrounds/edit', { campground });
   })
 );
@@ -61,6 +65,11 @@ router.put(
   isLoggedIn,
   catchAsync(async (req, res) => {
     const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash('error', 'You do not have permission to do that!');
+      return res.redirect(`/campgrounds/${id}`);
+    }
     const newData = req.body.campground;
     const updatedCampground = await Campground.findByIdAndUpdate(id, newData);
     req.flash('success', 'Succesfully updated campground!');
