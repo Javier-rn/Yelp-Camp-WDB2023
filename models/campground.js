119b8d5bf445f01512293f8f2b4fa0cacbe95632
Javier-rn <javier96rn@gmail.com>
2023-04-10 11:21:28 +0200

    Author added to campground model

--- models/campground.js
+++ models/campground.js
@@ -8,6 +8,10 @@ const CampgroundSchema = new Schema({
   imageURL: String,
   description: String,
   location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
   reviews: [
     {
       type: Schema.Types.ObjectId,
--- seeds/index.js
+++ seeds/index.js
@@ -22,14 +22,17 @@ const sample = (array) => {
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 30; i++) {
+    const author = '6431842c51d93d90f0e0630f';
     const random1000 = Math.floor(Math.random() * 1000) + 1;
-    const price= Math.floor(Math.random() * 20) + 10;
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       imageURL: await getRandomImage(),
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima et sequi quia molestias beatae similique commodi, ex laudantium inventore itaque accusamus aut. Ea ducimus asperiores temporibus incidunt recusandae, placeat accusamus.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima et sequi quia molestias beatae similique commodi, ex laudantium inventore itaque accusamus aut. Ea ducimus asperiores temporibus incidunt recusandae, placeat accusamus.',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      price
+      price,
+      author,
     });
     await camp.save();
   }
