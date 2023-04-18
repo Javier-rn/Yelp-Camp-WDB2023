d572bb0559cf414bd47b696e3520e4f46df5b1c0
Javier-rn <javier96rn@gmail.com>
2023-04-18 12:42:28 +0200

    Map is now centered to correct campground location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,5 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  zoom: 4,
+  center: campground.geometry.coordinates,
+  zoom: 8,
 });
+
+new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
